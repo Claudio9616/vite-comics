f8d22a25ad515d3d4ee2128212854ffd302478e3
Claudio9616 <clares9611@gmail.com>
2024-02-01 16:29:10 +0100

    add appheader to fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup>
-
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  name: 'Second app .Vue',
+  components: {
+    AppHeader
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+</template>
 
-<style scoped></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'APPHEADER',
+}
+</script>
+
+<template>
+    <header class="container">
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIE</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FAN</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </header>
+    <h1>guerhbvubu</h1>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid blue;
+}
+
+header ul {
+    width: 70%;
+    display: flex;
+    justify-content: space-between;
+    font-size: 1rem;
+    list-style-type: none;
+    border: 1px solid red;
+}
+
+ul li {
+    height: 100%;
+}
+</style>
